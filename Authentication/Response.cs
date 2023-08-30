50c2a77143676a932b5f5b53eb10e101103e099d
Lebohang Radebe <radebelebohang99@gmail.com>
2023-08-30 23:14:24 +0200

    Commit changes on program.cs file

---
+++ Authentication/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace Cmpg323_Project_2.Authentication
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+
+    }
+}
