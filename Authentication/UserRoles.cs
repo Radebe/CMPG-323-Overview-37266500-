79a8277f7dc4328af1a573690405d6a169a73ef0
Lebohang Radebe <radebelebohang99@gmail.com>
2023-08-30 23:14:24 +0200

    Commit changes on program.cs file

---
+++ Authentication/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace Cmpg323_Project_2.Authentication
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+
+    }
+}
